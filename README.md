9a250b3540a49f075835bde23bd4ed77e7ec0d91
ktneranga <49773078+ktneranga@users.noreply.github.com>
2020-04-21 03:12:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Traveler
+2nd-year semester 2 Mobile Application Development(Android) project
---
+++ app/src/main/java/com/example/traveler/activity_signin.java
@@ -0,0 +1,14 @@
+package com.example.traveler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_signin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
+}
